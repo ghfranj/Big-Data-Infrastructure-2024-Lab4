d44f3696cce8f4d6e20ca00951a68f6f25f9e171
asus <ghoghoghfran.jabour@gmail.com>
2024-04-25 16:17:21 +0300

    fix crlf

--- db/01-init.sh
+++ db/01-init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # Parse response to extract credentials
@@ -7,9 +7,9 @@ APP_DB_USER=$(curl -s -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_URL/v1/$VAULT_PATH
 APP_DB_NAME=$(curl -s -H "X-Vault-Token: $VAULT_TOKEN" $VAULT_URL/v1/$VAULT_PATH | jq -r '.data.data.DB_NAME')
 
 # Export fetched credentials as environment variables
-echo "got from vault $APP_DB_NAME"
-echo "got from vault $APP_DB_USER"
-echo "got from vault $APP_DB_PASS"
+#echo "got from vault $APP_DB_NAME"
+#echo "got from vault $APP_DB_USER"
+#echo "got from vault $APP_DB_PASS"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
    CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
