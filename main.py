aaadf7a09b7055cc8cf7f1c82835647191b857f2
asus <ghoghoghfran.jabour@gmail.com>
2024-04-27 00:06:07 +0300

    uncomment

--- main.py
+++ main.py
@@ -7,30 +7,30 @@ from Kafka.consumer import receive_from_kafka
 from Kafka.producer import send_to_kafka
 import sys
 if __name__ == '__main__':
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # print(f'Working on {device}')
-    #
-    # config = configparser.ConfigParser()
-    # config.read("config.ini")
-    #
-    # db = Database()  # Create an instance of the Database class
-    #
-    # if db.get_table_counts() < 25000:
-    #     process_data_tools.preprocess_data(db)
-    # else:
-    #     print("data found in database...")
-    #
-    # train_loader, test_loader, max_features = process_data_tools.build_loaders(db, int(config['CLASSIFIER_HYPERPARAMETERS']['batch_size']))
-    # print('Loaders built successfully')
-    #
-    # config['CLASSIFIER_HYPERPARAMETERS']['input_size'] = str(max_features)
-    # model = get_classifier(config)
-    # learning_rate = float(config['CLASSIFIER_HYPERPARAMETERS']['learning_rate'])
-    # optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate)
-    # criterion = torch.nn.BCELoss()
-    # epochs = int(config['CLASSIFIER_HYPERPARAMETERS']['epochs'])
-    # accuracy = train_model(model, train_loader, test_loader, optimizer, criterion, epochs=epochs)
-    accuracy = 0.7
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f'Working on {device}')
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    db = Database()  # Create an instance of the Database class
+
+    if db.get_table_counts() < 25000:
+        process_data_tools.preprocess_data(db)
+    else:
+        print("data found in database...")
+
+    train_loader, test_loader, max_features = process_data_tools.build_loaders(db, int(config['CLASSIFIER_HYPERPARAMETERS']['batch_size']))
+    print('Loaders built successfully')
+
+    config['CLASSIFIER_HYPERPARAMETERS']['input_size'] = str(max_features)
+    model = get_classifier(config)
+    learning_rate = float(config['CLASSIFIER_HYPERPARAMETERS']['learning_rate'])
+    optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate)
+    criterion = torch.nn.BCELoss()
+    epochs = int(config['CLASSIFIER_HYPERPARAMETERS']['epochs'])
+    accuracy = train_model(model, train_loader, test_loader, optimizer, criterion, epochs=epochs)
+
     # torch.save(model.state_dict(), config['MODEL']['classifier'])
     # model.load_state_dict(torch.load(config['MODEL']['classifier']))
     # print('Model stored successfully')
@@ -39,6 +39,6 @@ if __name__ == '__main__':
 
     # Receive messages from Kafka topic
     receive_from_kafka()
-    # db.close_connection()
+    db.close_connection()
     print("connection with database closed")
     # Close the database connection
